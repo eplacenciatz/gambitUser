64d62da09143e955f35e5b75061118ae267d4201
edwyn.placencia <Edwyn.Placencia@cignium.com>
2023-07-26 17:04:31 -0500

    Lambda en GO para manejo de usuarios

---
+++ db/signup.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/eplacenciatz/gambitUser/models"
+	"github.com/eplacenciatz/gambitUser/tools"
+)
+
+func SignUp(sig models.SignUp) error {
+	fmt.Println("Comienza Registro")
+
+	err := DbConnect()
+	if err != nil {
+		return err
+	}
+	defer Db.Close()
+
+	sentencia := "INSERT INTO users (User_Email, User_UUID, User_DateAdd) VALUES ('" + sig.UserEmail + "','" + sig.UserUUID + "','" + tools.FechaMySQL() + "')"
+	fmt.Println(sentencia)
+
+	_, err = Db.Exec(sentencia)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("SignUp > Ejecución Exitosa")
+	return nil
+}
